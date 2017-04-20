a984202f9c663767a85ca45915e79d3c7030a130
Budh Ram Gurung <budhram.gurung01@gmail.com>
2017-04-20 17:28:58 +0530

    Added fmt check

--- Makefile
+++ Makefile
@@ -3,6 +3,14 @@
 build:
 	go build server.go
 
+.PHONY: fmt
+fmt:
+	@gofmt -l -s -w .
+
+.PHONY: fmtcheck
+fmtcheck:
+	@gofmt -l -s . | read; if [ $$? == 0 ]; then echo "gofmt check failed for:"; gofmt -l -s .; exit 1; fi
+
 .PHONY: clean
 clean:
 	rm -f server
--- server.go
+++ server.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "io"
-  "fmt"
-  "net/http"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, "Hello world!")
+	io.WriteString(w, "Hello world!")
 }
 
 func main() {
-  http.HandleFunc("/", hello)
-  fmt.Println("Server is listing on port 8000.")
-  http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/", hello)
+	fmt.Println("Server is listing on port 8000.")
+	http.ListenAndServe(":8000", nil)
 }
