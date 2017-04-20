377792aad1303381b82d9cd5b532d1f453666c9f
Soumya Deb <debloper@gmail.com>
2017-04-20 19:27:25 +0530

    Updated server, fixes #2

--- server.go
+++ server.go
@@ -4,12 +4,19 @@ import (
   "fmt"
   "io"
   "net/http"
+  "strings"
 )
 
 const Port = 8000
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, "Hello world!")
+  text := r.URL.Path[1:]
+  text = strings.Title(text)
+
+  if text == "" {
+    text = "World"
+  }
+  io.WriteString(w, fmt.Sprintf("Hello, %s", text))
 }
 
 func main() {
